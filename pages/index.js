9bbc9ca805b900a36b3bbb386b90e39e2e9802af
Ignacio1996 <ec2-user@ip-10-11-203-135.ap-southeast-1.compute.internal>
2023-05-02 14:37:42 +0000

    Pushed from Locofy Builder

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,62 @@ const UserMessages = () => {
         <b className={styles.messages}>Messages</b>
         <div className={styles.conversation}>
           <MessageCard />
+          <div className={styles.contactCard}>
+            <div className={styles.photoautoLayout}>
+              <img
+                className={styles.photoautoLayoutChild}
+                alt=""
+                src="/ellipse-2@2x.png"
+              />
+              <div className={styles.nametext}>
+                <b className={styles.cameronWilliamson}>Cameron Williamson</b>
+                <div className={styles.omgThisIs}>omg, this is amazing</div>
+              </div>
+            </div>
+            <div className={styles.div}>14:32</div>
+          </div>
+          <div className={styles.contactCard}>
+            <div className={styles.photoautoLayout}>
+              <img
+                className={styles.photoautoLayoutChild}
+                alt=""
+                src="/ellipse-2@2x.png"
+              />
+              <div className={styles.nametext}>
+                <b className={styles.cameronWilliamson}>Cameron Williamson</b>
+                <div className={styles.omgThisIs}>omg, this is amazing</div>
+              </div>
+            </div>
+            <div className={styles.div}>14:32</div>
+          </div>
+          <div className={styles.contactCard}>
+            <div className={styles.photoautoLayout}>
+              <img
+                className={styles.photoautoLayoutChild}
+                alt=""
+                src="/ellipse-2@2x.png"
+              />
+              <div className={styles.nametext}>
+                <b className={styles.cameronWilliamson}>Cameron Williamson</b>
+                <div className={styles.omgThisIs}>omg, this is amazing</div>
+              </div>
+            </div>
+            <div className={styles.div}>14:32</div>
+          </div>
+          <div className={styles.contactCard}>
+            <div className={styles.photoautoLayout}>
+              <img
+                className={styles.photoautoLayoutChild}
+                alt=""
+                src="/ellipse-2@2x.png"
+              />
+              <div className={styles.nametext}>
+                <b className={styles.cameronWilliamson}>Cameron Williamson</b>
+                <div className={styles.omgThisIs}>omg, this is amazing</div>
+              </div>
+            </div>
+            <div className={styles.div}>14:32</div>
+          </div>
           <MessageCard />
           <MessageCard />
           <MessageCard />
